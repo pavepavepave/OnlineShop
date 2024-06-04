ac8d0b4c728607ff1e03680d5dee6788cfdcc80a
Pavel Yakovlev <deadamag@mail.ru>
2024-06-05 01:40:08 +0300

    Added statistical graphs

---
+++ OnlineShopWebApp/Areas/Admin/Controllers/StatisticController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Db.Interfaces;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using OnlineShopWebApp.Areas.Admin.Models;
+
+namespace OnlineShopWebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class StatisticController : Controller
+    {
+        private readonly IIncomeRepository _incomeRepository;
+        private readonly IExpenseRepository _expenseRepository;
+
+        public StatisticController(IIncomeRepository incomeRepository, IExpenseRepository expenseRepository)
+        {
+            _incomeRepository = incomeRepository;
+            _expenseRepository = expenseRepository;
+        }
+
+        public async Task<IActionResult> IncomeStatistics()
+        {
+            var incomes = await _incomeRepository.GetAllIncomesAsync();
+            return View(incomes.OrderByDescending(i => i.Date).ToList());
+        }
+
+        public async Task<IActionResult> ExpenseStatistics()
+        {
+            var expenses = await _expenseRepository.GetAllExpensesAsync();
+            return View(expenses.OrderByDescending(e => e.Date).ToList());
+        }
+        
+        public async Task<IActionResult> IncomeExpenseBalance()
+        {
+            var incomes = await _incomeRepository.GetAllIncomesAsync();
+            var expenses = await _expenseRepository.GetAllExpensesAsync();
+            var balanceData = new IncomeExpenseBalanceViewModel
+            {
+                Incomes = incomes.OrderByDescending(i => i.Date).ToList(),
+                Expenses = expenses.OrderByDescending(e => e.Date).ToList()
+            };
+            return View(balanceData);
+        }
+    }
+}
---
+++ OnlineShopWebApp/Areas/Admin/Models/IncomeExpenseBalanceViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using OnlineShop.Db.Models;
+
+namespace OnlineShopWebApp.Areas.Admin.Models
+{
+    public class IncomeExpenseBalanceViewModel
+    {
+        public List<Income> Incomes { get; set; }
+        public List<Expense> Expenses { get; set; }
+    }
+}
